5fae87ea79c937946e382edcf5050be9b70b4647
Dany <ionformat@gmail.com>
2020-04-30 10:30:49 +0200

    initialisation projet sts

---
+++ src/main/java/com/SPA/demo/ActorepApplication.java
@@ -0,0 +1,13 @@
+package com.SPA.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorepApplication.class, args);
+	}
+
+}
